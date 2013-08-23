42c577bfbe660d4d5739774446c9553d2d363f8f
Eric Myhre <hash@exultant.us>
2013-08-23 16:54:00 -0500

    One last touch to project layout basics: move the src directory to be a child of a .go folder, and adjust GOPATH="$PWD"/.go/ to match.
    This still doesn't affect the view of anyone linking this repo as a library.  It just makes me feel better -- not a single directory in my project root was forced on me by someone who hardcoded their zeal based on a belief they're better than their users.

--- go.test.sh
+++ go.test.sh
@@ -2,7 +2,7 @@
 
 function gotest {
 	package="$1"; shift
-	GORACE="log_path=test-$package-race.log" GOPATH=$PWD go test -race "polydawn.net/gosh/$package" "$@"
+	GORACE="log_path=test-$package-race.log" GOPATH=$PWD/.go/ go test -race "polydawn.net/gosh/$package" "$@"
 }
 
 gotest log
