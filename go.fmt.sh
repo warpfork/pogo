f82d8ef8037d34789f9f7725a77b1a56b5477db6
Eric Myhre <hash@exultant.us>
2013-08-24 13:38:54 -0500

    add go fmt wrapper script

---
+++ go.fmt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export GOPATH="$PWD"/.gopath/
+
+function gothing {
+	package="$1"; shift
+	go fmt "polydawn.net/gosh/$package" "$@"
+}
+
+gothing log
+gothing picnic
+gothing prom
+gothing psh
