0c6f2ee2cfbb01fe8b99f73ee0d243826cc76dfc
Eric Myhre <hash@exultant.us>
2013-08-25 02:44:37 -0500

    add writer conversions to iox package

--- iox/writer.go
+++ iox/writer.go
@@ -1,4 +1,86 @@
 package iox
 
 import (
+"fmt"
+	"bytes"
+	"io"
 )
+
+func WriterFromInterface(x interface{}) io.Writer {
+	switch y := x.(type) {
+	case []byte:
+		return WriterToByteSlice(y)
+	case io.Writer:
+		return y
+	case bytes.Buffer:
+		return &y
+	case chan<- string:
+		return WriterToChanString(y)
+	case chan<- []byte:
+		return WriterToChanByteSlice(y)
+	default:
+		return nil
+	}
+}
+
+func WriterToByteSlice(bats []byte) io.Writer {
+	return &writerByteSlice{slice: bats}
+}
+
+type writerByteSlice struct {
+	slice []byte
+}
+
+func (r *writerByteSlice) Write(p []byte) (n int, err error) {
+	c := cap(r.slice)
+	w := len(r.slice)
+	available := c - w
+	if len(p) < available {
+		available = len(p)
+	}
+	r.slice = r.slice[:w+available]
+	fmt.Printf(":: %d %d %d \n", w, c, available)
+	n = copy(r.slice[w:], p[:available])
+	fmt.Printf("::: %d \n", n)
+	return
+}
+
+func WriterToChanString(ch chan<- string) io.Writer {
+	return &writerChanString{ch: ch}
+}
+
+type writerChanString struct {
+	ch chan<- string
+}
+
+func (r *writerChanString) Write(p []byte) (n int, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			n = 0
+			err = io.EOF
+		}
+	}()
+
+	r.ch <- string(p)
+	return len(p), nil
+}
+
+func WriterToChanByteSlice(ch chan<- []byte) io.Writer {
+	return &writerChanByteSlice{ch: ch}
+}
+
+type writerChanByteSlice struct {
+	ch chan<- []byte
+}
+
+func (r *writerChanByteSlice) Write(p []byte) (n int, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			n = 0
+			err = io.EOF
+		}
+	}()
+
+	r.ch <- p
+	return len(p), nil
+}
---
+++ iox/writer_test.go
@@ -0,0 +1,100 @@
+package iox
+
+import (
+	"github.com/coocood/assrt"
+	"io"
+	"testing"
+	"sync"
+)
+
+func TestWriterToChanString(t *testing.T) {
+	assert := assrt.NewAssert(t)
+
+	ch := make(chan string)
+	w := WriterToChanString(ch)
+	go func() {
+		w.Write([]byte("asdf"))
+		w.Write([]byte(""))
+		w.Write([]byte("\nwakawaka"))
+		w.Write([]byte("\tz"))
+		close(ch)
+	}()
+
+	assert.Equal("asdf", <- ch)
+	assert.Equal("", <- ch)
+	assert.Equal("\nwakawaka", <- ch)
+	assert.Equal("\tz", <- ch)
+	_, open := <- ch
+	assert.Equal(false, open)
+}
+
+func TestWriterToChanStringClosed(t *testing.T) {
+	assert := assrt.NewAssert(t)
+
+	ch := make(chan string)
+	w := WriterToChanString(ch)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		n, err := w.Write([]byte("asdf"))
+		assert.Equal(4, n)
+		assert.Equal(nil, err)
+		close(ch)
+		n, err = w.Write([]byte("\tz"))
+		assert.Equal(0, n)
+		assert.Equal(io.EOF, err)
+		wg.Done()
+	}()
+
+	assert.Equal("asdf", <- ch)
+	_, open := <- ch
+	assert.Equal(false, open)
+	wg.Wait()
+}
+
+func TestWriterToChanByteSlice(t *testing.T) {
+	assert := assrt.NewAssert(t)
+
+	ch := make(chan []byte)
+	w := WriterToChanByteSlice(ch)
+	go func() {
+		w.Write([]byte("asdf"))
+		w.Write([]byte(""))
+		w.Write([]byte("\nwakawaka"))
+		w.Write([]byte("\tz"))
+		close(ch)
+	}()
+
+	assert.Equal([]byte("asdf"), <- ch)
+	assert.Equal([]byte(""), <- ch)
+	assert.Equal([]byte("\nwakawaka"), <- ch)
+	assert.Equal([]byte("\tz"), <- ch)
+	_, open := <- ch
+	assert.Equal(false, open)
+}
+
+func TestWriterToByteSlice(t *testing.T) {
+	assert := assrt.NewAssert(t)
+
+	bytes := make([]byte, 0, 9)
+	w := WriterToByteSlice(bytes)
+	go func() {
+		w.Write([]byte("asdf"))
+		w.Write([]byte(""))
+		w.Write([]byte("\nwakawaka"))
+		w.Write([]byte("\tz"))
+	}()
+
+	assert.Equal(
+		9,
+		len(bytes),
+	)
+	assert.Equal(
+		9,
+		cap(bytes),
+	)
+	assert.Equal(
+		"asdf\nwaka",
+		string(bytes),
+	)
+}
