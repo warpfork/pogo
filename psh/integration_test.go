c3949516a98ef36ac646557481089085a4661751
Eric Myhre <hash@exultant.us>
2013-08-26 18:49:55 -0500

    write a bunch of integration tests for psh.
    covers most of spawning commands and getting input and output to/from them.

---
+++ psh/integration_test.go
@@ -0,0 +1,142 @@
+package psh
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/coocood/assrt"
+	"testing"
+)
+
+var Printf = fmt.Printf
+
+func TestIntegration_ShReturns(t *testing.T) {
+	Sh("echo")()
+}
+
+func TestIntegration_CommandProvidesExitCode(t *testing.T) {
+	assert := assrt.NewAssert(t)
+
+	exitingShellCmd := Sh("bash")("-c")("exit 14").Start()
+	assert.Equal(
+		14,
+		exitingShellCmd.GetExitCode(),
+	)
+}
+
+func TestIntegration_ShCustomSuccessCodes(t *testing.T) {
+	// panics if the exit code opt doesnt work
+	Sh("bash")("-c")("exit 14")(Opts{OkExit: []int{14}})()
+}
+
+func TestIntegration_ShOutputWithStringChan(t *testing.T) {
+	assert := assrt.NewAssert(t)
+
+	out := make(chan string, 1)
+	Sh("echo")("wat")(Opts{Out: out})()
+	assert.Equal(
+		"wat\n",
+		<-out,
+	)
+}
+
+func TestIntegration_ShOutputWithByteSliceChan(t *testing.T) {
+	assert := assrt.NewAssert(t)
+
+	out := make(chan []byte, 1)
+	Sh("echo")("wat")(Opts{Out: out})()
+	assert.Equal(
+		[]byte("wat\n"),
+		<-out,
+	)
+}
+
+func TestIntegration_ShOutputWithBuffer(t *testing.T) {
+	assert := assrt.NewAssert(t)
+
+	var out bytes.Buffer
+	// note that we set opts with &out!  it's quite critical that that be a reference.
+	Sh("echo")("wat")(Opts{Out: &out})()
+	assert.Equal(
+		"wat\n",
+		out.String(),
+	)
+}
+
+func TestIntegration_ShInputWithString(t *testing.T) {
+	assert := assrt.NewAssert(t)
+
+	msg := "bees"
+	out := make(chan string, 1)
+	Sh("cat")("-")(Opts{In: msg, Out: out})()
+	assert.Equal(
+		msg,
+		<-out,
+	)
+}
+
+func TestIntegration_ShInputWithByteSlice(t *testing.T) {
+	assert := assrt.NewAssert(t)
+
+	msg := []byte("bees")
+	out := make(chan []byte, 1)
+	Sh("cat")("-")(Opts{In: msg, Out: out})()
+	assert.Equal(
+		msg,
+		<-out,
+	)
+}
+
+func TestIntegration_ShInputWithStringChan(t *testing.T) {
+	assert := assrt.NewAssert(t)
+
+	msg := "bees"
+	in := make(chan string, 1)
+	in <- msg
+	close(in)
+	out := make(chan string, 1)
+	Sh("cat")("-")(Opts{In: in, Out: out})()
+	assert.Equal(
+		msg,
+		<-out,
+	)
+}
+
+func TestIntegration_ShInputWithBuffer(t *testing.T) {
+	assert := assrt.NewAssert(t)
+
+	var in bytes.Buffer
+	msg := "bees"
+	in.WriteString(msg)
+	out := make(chan string, 1)
+	Sh("cat")("-")(Opts{In: msg, Out: out})()
+	assert.Equal(
+		msg,
+		<-out,
+	)
+}
+
+func TestIntegration_ShStreamingInputAndOutputWithStringChan(t *testing.T) {
+	assert := assrt.NewAssert(t)
+
+	msg1 := "bees\n"
+	msg2 := "knees\n"
+	in := make(chan string, 1)
+	out := make(chan string, 1)
+	catCmd := Sh("cat")("-")(Opts{In: in, Out: out}).Start()
+
+	in <- msg1
+	assert.Equal(
+		msg1,
+		<-out,
+	)
+	in <- msg2
+	assert.Equal(
+		msg2,
+		<-out,
+	)
+	close(in)
+	assert.Equal(
+		0,
+		catCmd.GetExitCode(),
+	)
+}
